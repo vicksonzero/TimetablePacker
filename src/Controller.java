f50d617be04f7536612661b6b09900ef7cefeff0
lalacarollucy95 <irisliu.0616@gmail.com>
2015-11-06 14:53:16 +0800

    changes in controllers

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+import meeting.Course;
+import meeting.Lecture;
+import meeting.Tutorial;
 import timetableGen.Timetable;
 
 
 public class Controller {
 
 	private ArrayList<Timetable> timetables=new ArrayList<Timetable>();
+	private static final Course course = new Course();
+	private static final Lecture lecture = new Lecture();
+	private static final Tutorial tutorial = new Tutorial();
+	private static final String FILENAME = "input-file.txt";
 	
 	public void main(String args[]){
 		//sort according to level of recommendation
