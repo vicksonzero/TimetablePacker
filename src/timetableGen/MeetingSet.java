0023d7361630f2927cbcb0f356a8a2b46a44a454
mayayat <sinyi812@gmail.com>
2015-11-09 01:16:25 +0800

    Update MeetingSet.java

--- src/timetableGen/MeetingSet.java
+++ src/timetableGen/MeetingSet.java
@@ -1,5 +1,19 @@
 package timetableGen;
+import java.util.ArrayList;
 
 public class MeetingSet {
 
+	public ArrayList<String> meetingset = new ArrayList<String>();
+	
+	public void generateMeetingSet(int coursenum){
+		for(int i=0; i<coursenum; i++){
+			meetingset.add(Course.courselist.get(i));
+		}
+	}
+	
+	
+	
+	
+
 }
+
