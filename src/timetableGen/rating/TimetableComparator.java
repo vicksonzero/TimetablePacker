246bd30949b03dd59b3ea1b819afa2fdddc398a9
vicksonzero <dicksonzero@gmail.com>
2015-12-10 21:30:30 +0800

    fixes crash when not enough candidate timetables
    also:
    removes unnecessary code that is never used

--- src/timetableGen/rating/TimetableComparator.java
+++ src/timetableGen/rating/TimetableComparator.java
@@ -4,15 +4,11 @@ import java.util.Comparator;
 
 import timetableGen.timetable.Timetable;
 
-public class TimetableComparator implements Comparator<Timetable> {
+public abstract class TimetableComparator implements Comparator<Timetable> {
 	/**
 	 * This is an empty sub-comparator (parent of other sub-comparator)
 	 */
 	public String name = "";
 
-	@Override
-	public int compare(Timetable o1, Timetable o2) {
-		return 0;
-	}
 
 }
